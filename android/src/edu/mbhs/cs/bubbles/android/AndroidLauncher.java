b33a28e10908cde83582f801e14891f50f85d11a
eyob-- <tse.eyob@gmail.com>
2015-12-17 20:09:51 -0500

    Add initial project skeleton
    Sub projects: Desktop, Android, iOS
    Extensions: Freetype, Controllers, Box2d, Box2dlights, Ai
    Third Party Extensions: VisUI, libgdx-utils, libgdx-utils-box2d,
    gdx-dialogs

---
+++ android/src/edu/mbhs/cs/bubbles/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package edu.mbhs.cs.bubbles.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import edu.mbhs.cs.bubbles.Bubbles;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Bubbles(), config);
+	}
+}
---
+++ desktop/src/edu/mbhs/cs/bubbles/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.mbhs.cs.bubbles.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.mbhs.cs.bubbles.Bubbles;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bubbles(), config);
+	}
+}
