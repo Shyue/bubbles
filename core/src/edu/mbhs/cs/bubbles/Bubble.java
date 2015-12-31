32f2f81ef81a4ed7677936dceefabf7976c3c3af
Gabe Udell <gabe.udell@gmail.com>
2015-12-31 16:10:02 -0500

    Reverted my merge by copying and pasting Eyob's code
    #ThinkBeforeYouSync
    #NeverAgain
    :(

--- core/src/edu/mbhs/cs/bubbles/Bubble.java
+++ core/src/edu/mbhs/cs/bubbles/Bubble.java
@@ -5,131 +5,117 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Touchable;
 
-public class Bubble extends Actor{
+/**
+ * Class representing the bubbles bouncing around the screen.
+ */
+public class Bubble extends Actor {
 	private float x = 0;
 	private float y = 0;
 	private float xVelocity;
 	private float yVelocity;
-	private int radius = 256/4;//pic size
-	private float r,g, b;//color floats
-	private float r_vel,g_vel,b_vel;//color velocities
+	private int radius = 64;	//pic size
+	private float r, g, b;	//color floats
+	private float r_vel, g_vel, b_vel;	//color velocities
+	private static final float TRANSPARENCY = 0.3f;
 	
 	Texture texture = new Texture(Gdx.files.internal("bubble.png"));
-	
+
+	/**
+	 * Places bubble at bottom left corner, gives it a radius of 64, gives it a random x and y velocity
+	 * between 0 and 10, sets rgb values to random values between 0 and 1, and sets rgb "velocities" to 0.5
+	 */
 	public Bubble(){
-		this.setBounds(x, y, radius*2, radius*2);
+		this.setBounds(x, y, radius * 2, radius * 2);
 		//This is a TERRIBLE way of doing things
-		r=(float)Math.pow(Math.random(),1.0/5);
-		g=(float)Math.pow(Math.random(),1.0/5);
-		b=(float)Math.pow(Math.random(),1.0/5);
-		xVelocity = (float)Math.random()*10;
-		yVelocity = (float)Math.random()*10;
+		r = (float)Math.pow(Math.random(), 1.0 / 5);
+		g = (float)Math.pow(Math.random(), 1.0 / 5);
+		b = (float)Math.pow(Math.random(), 1.0 / 5);
+		xVelocity = (float)Math.random() * 10;
+		yVelocity = (float)Math.random() * 10;
 		r_vel = 0.5f;
 		g_vel = 0.5f;
-		b_vel=0.5f;
-		this.setVisible(true);
-		
+		b_vel = 0.5f;
 	}
 	
 	@Override
 	public void act(float delta){
-		x+=xVelocity*delta;
-		y+=yVelocity*delta;
-		r+=r_vel*delta;
-		g+=g_vel*delta;
-		b+=b_vel*delta;
-		if(x>=this.getStage().getWidth()-140 || x<=0){
-			if(x<=0){
-				xVelocity = Math.abs(xVelocity);
-			}
-			else{
-				xVelocity = -Math.abs(xVelocity);
-			}
+		move(delta);
+		changeMotionVelocities(delta);
+		changeColor(delta);
+		changeColorVelocities(delta);
+
+		System.out.println(xVelocity);
+	}
+
+	/**
+	 * Moves the bubble in the x and y directions by its x and y velocities multiplied by delta
+	 * and reverses the direction the bubble is moving in if it hits a boundary
+	 * @param delta amount of "time" since last movement
+	 */
+	private void move(float delta) {
+		x += xVelocity * delta;
+		y += yVelocity * delta;
+
+		// if on edge, bounce
+		if(x >= this.getStage().getWidth() - 140 || x <= 0){
+			xVelocity *= -1;
 		}
-		if(y>=this.getStage().getHeight()-170 || y<=0){
-			if(y<=0){
-				yVelocity = Math.abs(yVelocity);
-			}
-			else{
-				yVelocity = -Math.abs(yVelocity);
-			}
+		if(y >= this.getStage().getHeight() - 170 || y <= 0){
+			yVelocity *= -1;
 		}
-		//somebody make a method
-		xVelocity+=200*Math.pow(1-Math.random(),3)*delta;
-		yVelocity+=200*Math.pow(1-Math.random(),3)*delta;
-		r_vel+=Math.pow(1-Math.random(),3)*delta;
-		g_vel+=Math.pow(1-Math.random(),3)*delta;
-		b_vel+=Math.pow(1-Math.random(),3)*delta;
-		if(r+g+b<1f){
-			r_vel+=0.5f;
-			g_vel+=0.5f;
-			b_vel+=0.5f;
+	}
+
+	/**
+	 * Changes the color of the bubble by the color velocities multiplied by delta
+	 * and reverses the "direction" of the color change if it hits the boundaries of 0 or 255
+	 * @param delta amount of "time" since last color change
+	 */
+	private void changeColor(float delta) {
+		r += r_vel * delta;
+		g += g_vel * delta;
+		b += b_vel * delta;
+
+		// keeping ourselves between 0 and 1
+		if(r < 0 || r > 1){
+			r_vel *= -1;
 		}
-		if(r<0)r_vel = 0.5f;
-		if(r>1)r_vel = -0.5f;
-		if(g<0)g_vel = 0.5f;
-		if(g>1)g_vel = -0.5f;
-		if(b<0)b_vel = 0.5f;
-		if(b>1)b_vel = -0.5f;
-		//System.out.println(xVelocity);
-		
-	}
-	public float getR() {
-		return r;
+		if(g < 0 || g > 1){
+			g_vel *= -1;
+		}
+		if(b < 0 || b > 1){
+			b_vel *= -1;
+		}
 	}
 
-	public float getG() {
-		return g;
+	/**
+	 * Changes the color velocities by a random number multiplied by delta
+	 * @param delta the amount of "time" since last color velocity change
+	 */
+	private void changeColorVelocities(float delta) {
+		r_vel += Math.pow(1 - Math.random(), 3) * delta;
+		g_vel += Math.pow(1 - Math.random(), 3) * delta;
+		b_vel += Math.pow(1 - Math.random(), 3) * delta;
+
+		if(r + g + b < 1){
+			r_vel += 0.5f;
+			g_vel += 0.5f;
+			b_vel += 0.5f;
+		}
 	}
 
-	public float getB() {
-		return b;
+	/**
+	 * Changes the x and y velocities by a random number multiplied by delta
+	 * @param delta the amount of "time" since last x-y velocity change
+	 */
+	private void changeMotionVelocities(float delta) {
+		xVelocity += 200 * Math.pow(1 - Math.random(), 3) * delta;
+		yVelocity += 200 * Math.pow(1 - Math.random(), 3) * delta;
 	}
 
-	public float getR_vel() {
-		return r_vel;
-	}
-
-	public float getG_vel() {
-		return g_vel;
-	}
-
-	public float getB_vel() {
-		return b_vel;
-	}
-
-	public void setR(float r) {
-		this.r = r;
-	}
-
-	public void setG(float g) {
-		this.g = g;
-	}
-
-	public void setB(float b) {
-		this.b = b;
-	}
-
-	public void setR_vel(float r_vel) {
-		this.r_vel = r_vel;
-	}
-
-	public void setG_vel(float g_vel) {
-		this.g_vel = g_vel;
-	}
-
-	public void setB_vel(float b_vel) {
-		this.b_vel = b_vel;
-	}
-
+	@Override
 	public void draw(Batch batch, float alpha){
-		batch.setColor(new Color(r,g,b,0.3f));
-		//batch.setColor(r,g,b,1f);
-		batch.draw(texture, x, y, radius*2,radius*2);
+		batch.setColor(new Color(r, g, b, TRANSPARENCY));
+		batch.draw(texture, x, y, radius * 2, radius * 2);
 	}
-	
 }
--- core/src/edu/mbhs/cs/bubbles/Bubbles.java
+++ core/src/edu/mbhs/cs/bubbles/Bubbles.java
@@ -3,29 +3,25 @@ package edu.mbhs.cs.bubbles;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
+/**
+ * Main Application: Bubbles Game
+ */
 public class Bubbles extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 	private static int BUBBLE_NUMBER = 20;
 	private Bubble[] b = new Bubble[BUBBLE_NUMBER];
 	private Stage stage;
+
 	@Override
 	public void create () {
 		stage = new Stage();
-		
-	//	batch = new SpriteBatch();
 		Gdx.app.log("AssetPath", Gdx.files.internal("assets/bubble.jpg").file().getAbsolutePath());
-		//img = new Texture("badlogic.jpg");
-		for(int i = 0; i<BUBBLE_NUMBER; i++){
+		for(int i = 0; i < 20; i++){
 			b[i] = new Bubble();
 			stage.addActor(b[i]);
 		}
-        Gdx.input.setInputProcessor(stage);
-		
+		stage.addActor(new Player());
 	}
 
 	@Override
