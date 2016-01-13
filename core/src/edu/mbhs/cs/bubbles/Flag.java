70f67877009de43e63ee6609ea0fc3c3113dbed4
Shyue <danielchenrules@gmail.com>
2016-01-13 09:01:40 -0500

    Added flag
    idk what this is

---
+++ core/src/edu/mbhs/cs/bubbles/Flag.java
@@ -0,0 +1,14 @@
+package edu.mbhs.cs.bubbles;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by guest1 on 1/11/2016.
+ */
+public class Flag extends Actor {
+    public Flag(){
+
+    }
+
+
+}
