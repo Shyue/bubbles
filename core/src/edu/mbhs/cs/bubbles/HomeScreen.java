b8e76a5cbe96fdeeb7b6b84246fe3799cb285682
eyob-- <tse.eyob@gmail.com>
2016-01-14 20:04:58 -0500

    Add Play Button Image

--- core/src/edu/mbhs/cs/bubbles/HomeScreen.java
+++ core/src/edu/mbhs/cs/bubbles/HomeScreen.java
@@ -4,18 +4,19 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 /**
  * Created by Eyob on 1/13/2016.
@@ -26,35 +27,50 @@ public class HomeScreen implements Screen {
 	private SpriteBatch batch;
 	private Stage stage;
 	private BitmapFont font;
-	private GlyphLayout layout;
-	private TextButtonStyle textButtonStyle;
+	private Button playButton;
+	private Drawable play, playClicked, playHovered;
 
 	public HomeScreen(Game g) {
 		game = g;
 		batch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.WHITE);
-		layout = new GlyphLayout();
+		GlyphLayout layout = new GlyphLayout();
 		layout.setText(font, "Bubbles");
+		play = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("play.jpg"))));
+		playClicked = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("play-clicked.jpg"))));
+		playHovered = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("play-hovered.jpg"))));
 	}
 
 	@Override
 	public void show() {
+		ButtonStyle style = new ButtonStyle();
+		style.down = playClicked;
+		style.up = play;
+
 		stage = new Stage();
-		textButtonStyle = new TextButtonStyle();
-		textButtonStyle.font = font;
-		TextButton b = new TextButton("play", textButtonStyle);
-		b.setSize(50, 50);
-		b.setBounds(stage.getWidth() / 2 - widthOf("play") / 2, stage.getHeight() / 2 - 50, widthOf("play"), 50);
-		System.out.println(widthOf("play"));
-		b.addListener(new ClickListener() {
+		playButton = new Button(style);
+		playButton.setBounds(stage.getWidth() / 2 - 50, stage.getHeight() / 2 - 50, 100, 50);
+		playButton.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				super.clicked(event, x, y);
 				game.setScreen(new Bubbles());
 			}
+
+			@Override
+			public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+				super.enter(event, x, y, pointer, fromActor);
+				changeButton(playHovered);
+			}
+
+			@Override
+			public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+				super.exit(event, x, y, pointer, toActor);
+				changeButton(play);
+			}
 		});
-		stage.addActor(b);
+		stage.addActor(playButton);
 		Gdx.input.setInputProcessor(stage);
 	}
 
@@ -64,7 +80,12 @@ public class HomeScreen implements Screen {
 		font.draw(batch, "Bubbles", Gdx.graphics.getWidth() / 2 - widthOf("Bubbles") / 2,
 				Gdx.graphics.getHeight() / 2 + 50);
 		batch.end();
+		stage.act(delta);
 		stage.draw();
+	}
+
+	private void changeButton(Drawable d) {
+		playButton.getStyle().up = d;
 	}
 
 	private float widthOf(String s) {
