16e8c7c3a8a0dbf8f54148e69bf2af45a0e801b3
Gabe Udell <gabe.udell@gmail.com>
2016-01-20 11:06:29 -0500

    Music plays
    We should figure out how to pause and or save on android

--- core/src/edu/mbhs/cs/bubbles/BubblesMain.java
+++ core/src/edu/mbhs/cs/bubbles/BubblesMain.java
@@ -1,14 +1,28 @@
 package edu.mbhs.cs.bubbles;
 
+import com.badlogic.gdx.Audio;
 import com.badlogic.gdx.Game;
-
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 /**
  * Created by Eyob on 1/13/2016.
  */
 public class BubblesMain extends Game {
-
+	public Music music;
 	@Override
 	public void create() {
 		setScreen(new HomeScreen(this));
+		music = Gdx.audio.newMusic(Gdx.files.internal("Musick.mp3"));
+		music.play();
+		music.setLooping(true);
+	}
+	@Override
+	public void pause() {
+		music.pause();
+	}
+
+	@Override
+	public void resume() {
+		music.play();
 	}
 }
